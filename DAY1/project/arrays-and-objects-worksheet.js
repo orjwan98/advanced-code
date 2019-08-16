01dc76b87421f104cbd781aee5ae89b7b2796acf
Orjwan Al-Rajaby <orjwan98@hotmail.com>
2019-08-16 20:47:52 +0300

    make challeges for rest and spread operators

--- DAY1/project/arrays-and-objects-worksheet.js
+++ DAY1/project/arrays-and-objects-worksheet.js
@@ -84,3 +84,94 @@ var people = {
 //[{name: 'amelie', hobbie: 'drawing}, {name: 'jackson', hobbie: 'cooking}, {name: 'sarah', hobbie:'reading & writing'}, {name:'mike', hobbie:'listening to music'}, {name: 'martha', hobbie:'knitting'}]
 
 //-------------------------------------------------
+
+//challenge6: using the rest and spread operators ONLY, solve the following challenges:
+
+//1. write a function that takes two arrays as arguements and returns one big array containing elements from both of these arrays.
+
+var numArr = [1, 2, 3];
+var namesArr = ['Sammi', 'Arwa', 'Maraam', 'Mahmoud'];
+
+function concatArrs(arr1, arr2) {
+  //write your code here
+
+  return bigArr;
+}
+
+//should return
+
+// bigArr = [1, 2, 3, 'Sammi', 'Arwa', 'Maraam', 'Mahmoud']
+
+//------------------------------------------
+
+//2. write a function that takes an array and a word as arguments and returns a new copy of the array with the word as an extra element at the end.
+
+var fruits = ['Apple', 'Orange', 'Kiwi', 'Lemon'];
+
+function newArrays(word, arr) {
+  //write your code here
+
+  return newArr;
+}
+
+//should return
+
+//newArr = ['Apple', 'Orange', 'Kiwi', 'Lemon', 'banana']
+
+//------------------------------------------
+
+//3. write a function that takes two objects as an argument and returns one big object that has all properties and values of both objects.
+
+var company = {
+  company: 'Intersect Ltd.',
+  location: 'Germany'
+};
+
+var employee = {
+  name: 'Mark Miles',
+  age: 32,
+  email: 'mark@intersect_ltd.com'
+};
+
+function concatObjs(obj1, obj2) {
+  //write your code here
+
+  return bigObj;
+}
+
+//should return
+
+//bigObj = {
+//  name: 'Mark Miles',
+//  age: 32,
+//  email: 'mark@intersect_ltd.com',
+//  company: 'Intersect Ltd.',
+//  location: 'Germany'
+//}
+
+//------------------------------------
+
+//4. write a function that takes an object as an argument, and returns a copy of the object that has a new property and value. The property and value can be anything you want.
+
+var laptop = {
+  modelName: 'Lenovo ThinkPad',
+  modelNum: 'L440',
+  ram: '4GB',
+  hdd: '500GB'
+};
+
+function returnNewObj() {
+  //write your code here
+
+  return newObj;
+}
+
+//should return
+
+//newObj = {
+//  modelName: 'Lenovo ThinkPad',
+//  modelNum: 'L440',
+//  ram: '4GB',
+//  hdd: '500GB',
+//  cpu: 'i5 intel core'
+//}
