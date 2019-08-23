24127c03b427f1fe48d9a7f0b9707bc812903093
Orjwan <orjwan98@hotmail.com>
2019-08-23 15:16:30 +0300

    make fetch demo to explain how fetch works

---
+++ DAY2/fetch-demo/request-response-cycle.js
@@ -0,0 +1,19 @@
+//fetch demo
+
+fetch('https://picsum.photos/v2/list')
+  .then(function(response) {
+    console.log('I will run last');
+    console.log(response);
+    var parsed = response.json();
+    return parsed;
+  })
+  .then(function(parsed) {
+    console.log(parsed);
+  })
+  .catch(function(error) {
+    console.log(error);
+  });
+
+console.log('I will run first');
+
+console.log('I will run second');
