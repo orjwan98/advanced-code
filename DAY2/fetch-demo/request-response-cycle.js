1593290c8a67fe8cd3e577680c7334442eee1f3a
Orjwan <orjwan98@hotmail.com>
2019-08-24 02:12:09 +0300

    make fetch demo

--- DAY2/fetch-demo/request-response-cycle.js
+++ DAY2/fetch-demo/request-response-cycle.js
@@ -1,19 +1,21 @@
-//fetch demo
+// //fetch demo
 
-fetch('https://picsum.photos/v2/list')
-  .then(function(response) {
-    console.log('I will run last');
-    console.log(response);
-    var parsed = response.json();
-    return parsed;
-  })
-  .then(function(parsed) {
-    console.log(parsed);
-  })
-  .catch(function(error) {
-    console.log(error);
-  });
+// fetch('https://picsum.photos/v2/list')
+//   .then(function(response) {
+//     console.log('I will run last');
+//     console.log(response);
+//     var parsed = response.json();
+//     return parsed;
+//   })
+//   .then(function(parsed) {
+//     console.log(parsed);
+//   })
+//   .catch(function(error) {
+//     console.log(error);
+//   });
 
-console.log('I will run first');
+// console.log('I will run first');
 
-console.log('I will run second');
+// console.log('I will run second');
+
+// console.log(fetch('https://picsum.photos/v2/list'));
