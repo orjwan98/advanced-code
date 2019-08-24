42a5a5008468aa7c26098f1902ea25cd19c87ade
aboud20-meet <aboud20@meet.mit.edu>
2019-08-24 15:29:32 +0300

    make API call, update state and import Form and Products

--- DAY2/makeup-search/src/App.js
+++ DAY2/makeup-search/src/App.js
@@ -1,12 +1,42 @@
 import React from 'react';
 import './App.css';
+import Form from './components/Form'
+import Products from './components/Products'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(){
+    super(); 
+    this.state = {
+      data:''
+    }
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick(searchInput){
+    fetch(`http://makeup-api.herokuapp.com/api/v1/products.json?product_type=${searchInput}`)
+    .then(function(response){
+      return response.json();
+    })
+    .then(function(resData){
+      this.setState({data:resData})
+    }.bind(this))
+    .catch(function(error){
+      console.log(error)
+    })
+  }   
+
+
+  render(){
+    return (
+      <div className="App">
+        <h1>Hello World</h1>
+        <Form handleClick={this.handleClick}/>
+        <Products data={this.state.data}/>
+      </div>
+    );
+  }
+
 }
 
 export default App;
