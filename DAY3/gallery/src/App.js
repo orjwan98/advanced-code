0c37591dc9b15366a3f3282506830b9cff77b058
Orjwan <orjwan98@hotmail.com>
2019-08-29 14:43:42 +0300

    define state in App and define clickHandler

--- DAY3/gallery/src/App.js
+++ DAY3/gallery/src/App.js
@@ -4,15 +4,32 @@ import Form from './components/Form';
 import Gallery from './components/Gallery'
 
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Photo Gallery</h1>
-      <h2>Add your favorite images!</h2>
-      <Form />
-      <Gallery/>
-    </div>
+class App extends React.Component{
+
+  constructor() {
+    super(); 
+    this.state = {
+      images: []
+    }
+
+    this.handleClick = this.handleClick.bind(this)
+  }
+  
+
+  handleClick(imageInput) {
+    console.log(imageInput)
+  }
+
+  render(){
+     return (
+      <div className="App">
+        <h1>Photo Gallery</h1>
+        <h2>Add your favorite images!</h2>
+         <Form handleClick={this.handleClick}/>
+        <Gallery/>
+      </div>
   );
+ }
 }
 
 export default App;
