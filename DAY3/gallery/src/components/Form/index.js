9714532c7074ee9473d168b5102d87d8682f0242
Orjwan <orjwan98@hotmail.com>
2019-08-29 14:19:21 +0300

    make Form class component and add state

---
+++ DAY3/gallery/src/components/Form/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Form extends React.Component {
+
+    state = {
+        imageInput: ''
+    }
+
+    render() {
+        return (
+            <div>
+                <label htmlFor="image-input">
+                    <input type="text" id="image-input"/>
+                </label>
+                <button type="button">ADD</button>
+            </div>
+        )
+    }
+}
