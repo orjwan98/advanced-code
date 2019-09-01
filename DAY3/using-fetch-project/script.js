45cba4f2cca3f0284e47ad6b8d41672c67dc15f8
Orjwan <orjwan98@hotmail.com>
2019-09-01 10:54:13 +0300

    make fetch project

---
+++ DAY3/using-fetch-project/script.js
@@ -0,0 +1,12 @@
+console.log('Hello fetch');
+
+//Write a function that takes the name of a character as an argument, and uses fetch to make a request to the rick and morty characters API (), and returns:
+
+//1. All characters.
+//Hint: you can't see the data inside the response unless you parse the response.
+
+//2. Only characters that are called 'rick'.
+
+//3. Only characters that are called 'morty'.
+
+//Refer to the rick and morty documentation to learn how to filter your requests results: ()
