ac207c6fc5128027ff4f4c01b26690d15c6a75bc
Orjwan <orjwan98@hotmail.com>
2019-09-02 19:25:59 +0300

    add arrow functions explanation

---
+++ DAY3/arrow-functions.js
@@ -0,0 +1,22 @@
+//ES6 arrow functions
+
+//normal ES5 function
+function sayHi() {
+  console.log('hiiiiiiiii');
+}
+
+sayHi();
+
+//store anonymous function in a variable
+var sayHello = function() {
+  console.log('Hello');
+};
+
+sayHello();
+
+//store anonymous arrow function in a variable
+var sayHey = () => {
+  console.log('sayHey');
+};
+
+sayHey();
