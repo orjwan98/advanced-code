6ff356e82202e5917b6ddc4fd6d3d709d1a1a5f5
Orjwan <orjwan98@hotmail.com>
2019-09-20 15:05:55 +0300

    fix name from Form to Photo

--- DAY3/gallery/src/components/Photo/index.js
+++ DAY3/gallery/src/components/Photo/index.js
@@ -1,9 +1,9 @@
 import React from 'react'; 
 
-var Form = (props) => {
+var Photo = (props) => {
     return (
         <img src={props.imageLink}/>
     )
 }
 
-export default Form;
+export default Photo;
